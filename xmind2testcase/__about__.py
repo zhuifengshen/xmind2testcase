2e3ad94ea0b77de2a174bd5f7bd2a5d5d3bc33db
yoyoalphax <xzn_alpha@hotmail.com>
2022-03-04 13:48:00 +0800

    1、添加版本说明 2、更新Tag

--- xmind2testcase/__about__.py
+++ xmind2testcase/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'xmind2testcase'
 __description__ = 'XMind2TestCase基于Python实现，提供了一个高效测试用例设计的解决方案！'
 __keywords__ = 'xmind2testCase, testcase, test, testing, xmind, 思维导图, XMind思维导图',
-__url__ = 'https://github.com/zhuifengshen/xmind2testcase'
+__url__ = 'https://github.com/yoyoalphax/xmind2testcase'
 __author__ = 'Devin'
 __author_email__ = '1324556701@qq.com'
 __version__ = '1.5.1'
